866c27158d68d4b00ea0ece1cf1bed0a004ad172
Sumi Gunyu <sumkakkyo@gmail.com>
2024-04-09 00:50:33 +0900

    348A問題

---
+++ ABC086/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a;
+  int b;
+  cin >> a;
+  cin >> b;
+
+  if (a % 2 != 0 && b % 2 != 0) {
+    cout << "Odd";
+  } else {
+    cout << "Even";
+  }
+
+}
