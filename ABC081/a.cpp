f1cac75b27c70752f16cf77d5153fe447adc33bf
Sumi Gunyu <sumkakkyo@gmail.com>
2024-04-09 21:31:07 +0900

    add: ABC081A

--- ABC081/a.cpp
+++ ABC081/a.cpp
@@ -3,15 +3,11 @@
 using namespace std;
 
 int main(){
-  int a;
-  int b;
-  cin >> a;
-  cin >> b;
-
-  if (a % 2 != 0 && b % 2 != 0) {
-    cout << "Odd";
-  } else {
-    cout << "Even";
+  string s;
+  cin >> s;
+  int n = 0;
+  for (int i =0; i < 3; i++) {
+    if (s[i] == '1') ++n;
   }
-
+  cout << n << endl;
 }
