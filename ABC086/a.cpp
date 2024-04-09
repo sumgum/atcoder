c65f991b813a6b8da4ce0b7227a8524fdbca4207
Sumi Gunyu <sumkakkyo@gmail.com>
2024-04-09 20:58:10 +0900

    update: 348A

--- ABC086/a.cpp
+++ ABC086/a.cpp
@@ -3,15 +3,9 @@
 using namespace std;
 
 int main(){
-  int a;
-  int b;
-  cin >> a;
-  cin >> b;
-
-  if (a % 2 != 0 && b % 2 != 0) {
-    cout << "Odd";
-  } else {
-    cout << "Even";
-  }
-
+  int a, b;
+  cin >> a >>b;
+  int c = a * b;
+  if (c % 2 == 0) cout << "Even" << endl;
+  else cout << "Odd" << endl;
 }
