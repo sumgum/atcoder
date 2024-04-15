23d309bf77c2e12af57d9bba265d3af0b31baa52
Sumi Gunyu <sumkakkyo@gmail.com>
2024-04-15 22:13:48 +0900

    update: ABC349B

--- ABC349/b.cpp
+++ ABC349/b.cpp
@@ -1,48 +1,22 @@
 #include <bits/stdc++.h>
 #include <iostream>
 #include <algorithm>
+#include <map>
 using namespace std;
 
 int main(){
-  string S;
-  cin >> S;
-  vector<char> C(S.size());
-  vector<int> Streak;
-  Streak.reserve(S.size());
-
-  for (int i = 0; i < S.size(); i++) {
-    C[i] = S[i];
-  }
-  sort(C.begin(), C.end());
-
-  // for (int i = 0; i < S.size(); i++) {
-  //   cout << C[i] << endl;
-  // }
-
-  int streak = 1;
-  bool isYes = true;
-  for (int i = 0; i < S.size()-1; i++) {
-      if (C[i] == C[i+1]) {
-        streak++;
-      } else {
-        Streak.push_back(streak);
-        streak = 1;
-      }
-  }
-
-  for (int i = 0; i < Streak.size(); i++) {
-    // Streakに含まれるi(1からStreak.size()までの整数)の数が0または2であることを確認する
-    // iの数を取得する
-    int count = std::count(Streak.begin(), Streak.end(), i);
-    if (count != 0 && count != 2) {
-      isYes = false;
+  string s;
+  cin >> s;
+  map<char,int> cnt;
+  for (char c: s) cnt[c]++;
+  map<int,int> cnt2;
+  for (auto p : cnt) cnt2[p.second]++;
+  for (auto p : cnt2) {
+    if (p.second != 2) {
+      cout << "No" << endl;
+      return 0;
     }
   }
-
-  if (isYes) {
-    cout << "Yes" << endl;
-  } else {
-    cout << "No" << endl;
-  }
-
+  cout << "Yes" << endl;
+  return 0;
 }
