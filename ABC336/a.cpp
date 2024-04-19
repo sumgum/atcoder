053a072d77a04f5be260f890beaa3a095b467804
Sumi Gunyu <sumkakkyo@gmail.com>
2024-04-19 12:34:59 +0900

    add: ABC336A

---
+++ ABC336/a.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include <iostream>
+#include <regex>
+using namespace std;
+
+int main(){
+  int n;
+  cin>>n;
+  cout<<"L";
+  for (int i = 0; i < n; i++) {
+    cout<<"o";
+  }
+  cout<<"ng"<<endl;
+}
