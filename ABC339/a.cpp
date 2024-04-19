9f55240359468e39bf72f459790e43ce4d3988e6
Sumi Gunyu <sumkakkyo@gmail.com>
2024-04-19 11:46:45 +0900

    update: ABC339A

--- ABC339/a.cpp
+++ ABC339/a.cpp
@@ -2,29 +2,10 @@
 #include <iostream>
 using namespace std;
 
-vector<string> split_native(const string &s, char delim) {
-  vector<string> elems;
-  string item;
-  for(char ch: s) {
-    if (ch == delim) {
-      if (!item.empty())
-        elems.push_back(item);
-      item.clear();
-    }
-    else {
-      item += ch;
-    }
-  }
-  if (!item.empty())
-    elems.push_back(item);
-  return elems;
-}
-
 int main(){
   string s;
   cin>>s;
   
-  vector<string> a(s.size());
-  a = split_native(s,'.');
-  cout<<a[a.size()-1];
+  int n = s.rfind('.');
+  cout<<s.substr(n+1);
 }
