fd3c3fad9516410f8641ee3da882eb960dec05d3
Sumi Gunyu <sumkakkyo@gmail.com>
2024-04-19 12:39:59 +0900

    add: ABC335A

---
+++ ABC335/a.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include <iostream>
+#include <regex>
+using namespace std;
+
+int main(){
+  string s;
+  cin>>s;
+  int n = s.size();
+
+  cout<<s.substr(0,n-1)<<4<<endl;
+}
