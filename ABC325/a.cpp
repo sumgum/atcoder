6b4b6583caf6c8a706834d50447ffa62303d290d
Sumi Gunyu <sumkakkyo@gmail.com>
2024-05-05 21:58:19 +0900

    add: ABC325

---
+++ ABC325/a.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include <iostream>
+#include <regex>
+using namespace std;
+#define rep(i,n) for (int i = 0; i < n; ++i)
+using ll = long long;
+using P = pair<int,int>;
+
+int main(){
+  string s;
+  cin >> s;
+  cout << s << " san" << endl;
+}
