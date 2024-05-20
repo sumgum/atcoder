bd72593cf5531ccc6e6cde9ad36f22ee7359778e
Sumi Gunyu <sumkakkyo@gmail.com>
2024-05-21 06:24:33 +0900

    add: ABC313

--- ABC313/a.cpp
+++ ABC313/a.cpp
@@ -1,14 +1,24 @@
 #include<bits/stdc++.h>
 #include <iostream>
 #include <regex>
+#include <algorithm>
 using namespace std;
 #define rep(i,n) for (int i = 0; i < n; ++i)
 using ll = long long;
 using P = pair<int,int>;
 
 int main(){
-  string s = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
   int n;
   cin >> n;
-  cout << s.substr(0,n+2) << endl;
+  vector<int> p(n);
+  rep(i,n) cin >> p[i];
+  int result = 0;
+  rep(i,n) {
+    if (i != 0) {
+      result = max(result, p[i] -p[0] + 1);
+    }
+  }
+
+  cout << result << endl;
 }
+
